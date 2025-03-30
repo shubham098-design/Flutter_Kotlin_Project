2e05cfba16a65ad95a08b052bc8492baa41b3fe9
shubham098-design <114378272+shubham098-design@users.noreply.github.com>
2025-03-30 16:48:29 +0500

    Update cart.js

--- eccomerce_by_rivvan/server/models/cart.js
+++ eccomerce_by_rivvan/server/models/cart.js
@@ -1,33 +1,11 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    products: [
-      {
-        productId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-          required: true,
-        },
-        quantity: { type: Number, default: 1 },
-      },
-    ],
-    amount: { type: Number, required: true },
-    address: { type: Object, required: true },
-    status: {
-      type: String,
-      default: "pending",
-      enum: ["pending", "shipped", "delivered", "cancelled"],
-    },
-  },
-  { timestamps: true } // ✅ Automatically adds `createdAt` & `updatedAt`
-);
+const cartSchema = new mongoose.Schema({
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    quantity: { type: Number, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    createdAt: { type: Date, default: Date.now },
+});
 
-const Order = mongoose.model("Order", orderSchema);
-
-module.exports = Order;
+const Cart = mongoose.model("Cart", cartSchema);
+module.exports = Cart;
