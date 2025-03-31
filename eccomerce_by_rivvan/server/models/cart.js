1fafd6c83fef3836d57d3169a16d27c550b6590e
shubham098-design <114378272+shubham098-design@users.noreply.github.com>
2025-03-31 07:49:23 +0500

    Update cart.js

--- eccomerce_by_rivvan/server/models/cart.js
+++ eccomerce_by_rivvan/server/models/cart.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
-    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-    quantity: { type: Number, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    createdAt: { type: Date, default: Date.now },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: { type: Number, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Cart = mongoose.model("Cart", cartSchema);
