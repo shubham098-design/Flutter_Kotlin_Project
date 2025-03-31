5d598b10262a31b3bc6495356698a5ca2d1eb0c5
shubham098-design <114378272+shubham098-design@users.noreply.github.com>
2025-03-31 16:33:44 +0500

    Update auth.js

--- eccomerce_by_rivvan/server/routes/auth.js
+++ eccomerce_by_rivvan/server/routes/auth.js
@@ -1,130 +1,35 @@
+// import packages
 const express = require("express");
-const User = require("../models/user");
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
-const authRouter = express.Router();
+// instantiate express
+const app = express();
+const DB =
+  "mongodb+srv://shubhamdhaniyan:shu251203@cluster0.z1uay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-authRouter.post("/api/signup", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-    const hashedPassword = await bcryptjs.hash(password, 8);
+// import files
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const cartRouter = require("./routes/cart");
+const orderRouter = require("./routes/order");
+const userRouter = require("./routes/user");
 
-    let user = new User({ name, email, password: hashedPassword });
-    await user.save();
-    res.status(201).json({ data: user, message: "User created successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
+// middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(productRouter);
+app.use(cartRouter);
+app.use(orderRouter);
+app.use(userRouter);
+// create server
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-authRouter.post("/api/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "User does not exist" });
-    }
-    const isPasswordValid = await bcryptjs.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-    // res.status(200).json({ data: user, message: "Login successful" });
-
-    // extra work with rivan  bassically add the token in the response because we need when we are navigate the user login to home screen
-
-    const token = jwt.sign({ id: user._id }, "passwordKey");
-    res.status(200).json({ token, ...user._doc });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
-
-authRouter.post("/tokenIsValid", async (req, res) => {
-  try {
-    const token = req.header("x-auth-token");
-    if (!token) return res.json(false);
-    const verified = jwt.verify(token, "passwordKey");
-    if (!verified) return res.json(false);
-    const user = await User.findById(verified.id);
-    if (!user) return res.json(false);
-    return res.json(true);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
-
-authRouter.get("/", async (req, res) => {
-  const user = await User.findById(req.user);
-  res.json({ ...user._doc, token: req.token });
-});
-
-authRouter.get("/", async (req, res) => {
-  res.send("hello World");
-});
-
-authRouter.get("/api/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res
-      .status(200)
-      .json({ data: users, message: "Users fetched successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
-
-authRouter.get("/api/users/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    res.status(200).json({ data: user, message: "User fetched successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Something went wrong" });
-  }
-});
-
-
-authRouter.put("/api/users/:id", async (req, res) => {
-  try {
-    const { name, email, address, type } = req.body; // Extract only user details (Not Cart)
-    
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // Update only the specified fields
-    if (name) user.name = name;
-    if (email) user.email = email;
-    if (address) user.address = address;
-    if (type) user.type = type;
-
-    const updatedUser = await user.save();
-
-    res.status(200).json({ data: updatedUser, message: "User updated successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Something went wrong" });
-  }
-});
-
-authRouter.delete("/api/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    res.status(200).json({ data: user, message: "User deleted successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Something went wrong" });
-  }
-});
-
-module.exports = authRouter;
+app.listen(3000, () => console.log("http://localhost:3000"));
+module.exports = app;
